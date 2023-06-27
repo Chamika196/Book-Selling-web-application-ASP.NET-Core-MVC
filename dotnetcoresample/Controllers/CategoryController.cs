3bcab137535c7aa3e19c2623dde3ade62c9fed16
Chamika196 <chamikarathnayaka1996@gmail.com>
2023-06-27 12:03:01 +0530

    Add project files.

---
+++ dotnetcoresample/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using dotnetcoresample.Data;
+using dotnetcoresample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcoresample.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //retreiving data 
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+        
+        public IActionResult Create ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot Exactly the same Display Name");
+            }
+            //if (obj.Name != null && obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("name", "Test is an invalid value");//error message near name tag
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+           
+        }
+    }
+}
---
+++ dotnetcoresample/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using dotnetcoresample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetcoresample.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+                
+        }
+
+        public DbSet <Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action1", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Action2", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Action3", DisplayOrder = 3 }
+                );
+        }
+    }
+}
